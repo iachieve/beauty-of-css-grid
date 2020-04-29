2d9ccb7a358f44a069554c7e81a016f8fdccff3a
iachieve <mhalim80@hotmail.com>
2020-04-29 04:53:39 -0700

    add colors & typography constants

--- portfolio-1/gulpfile.js
+++ portfolio-1/gulpfile.js
@@ -1,29 +1,71 @@
-const gulp = require('gulp'),
-  browserSync = require('browser-sync').create(),
-  sass = require('gulp-sass');
+// Initialize modules
+// Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
+const { src, dest, watch, series, parallel } = require('gulp');
+// Importing all the Gulp-related packages we want to use
+const sourcemaps = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+var replace = require('gulp-replace');
 
 
-// Static server & watch scss + html files
-gulp.task('watch', ['sass'], function () {
+// File paths
+const files = {
+  scssPath: './scss/**/*.scss',
+  jsPath: 'app/js/**/*.js'
+}
 
-  browserSync.init({
-    server: '.'
-  });
+// Sass task: compiles the style.scss file into style.css
+function scssTask() {
+  return src(files.scssPath)
+    .pipe(sourcemaps.init()) // initialize sourcemaps first
+    .pipe(sass()) // compile SCSS to CSS
+    .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
+    .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
+    .pipe(dest('dist')
+    ); // put final CSS in dist folder
+}
 
-  gulp.watch('./scss/**/*.scss', ['sass'], browserSync.reload);
-  gulp.watch('./*.html').on('change', browserSync.reload);
-  gulp.watch('./js/**/*.js', browserSync.reload);
+// JS task: concatenates and uglifies JS files to script.js
+function jsTask() {
+  return src([
+    files.jsPath
+    //,'!' + 'includes/js/jquery.min.js', // to exclude any specific files
+  ])
+    .pipe(concat('all.js'))
+    .pipe(uglify())
+    .pipe(dest('dist')
+    );
+}
 
-});
+// Cachebust
+function cacheBustTask() {
+  var cbString = new Date().getTime();
+  return src(['index.html'])
+    .pipe(replace(/cb=\d+/g, 'cb=' + cbString))
+    .pipe(dest('.'));
+}
 
-// Compile Sass into CSS & inject into browsers
-gulp.task('sass', function () {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
-});
+// Watch task: watch SCSS and JS files for changes
+// If any change, run scss and js tasks simultaneously
+function watchTask() {
+  watch([files.scssPath, files.jsPath],
+    { interval: 1000, usePolling: true }, //Makes docker work
+    series(
+      parallel(scssTask, jsTask),
+      cacheBustTask
+    )
+  );
+}
 
-
-// default will also watch
-gulp.task('default', ['watch']);
+// Export the default Gulp task so it can be run
+// Runs the scss and js tasks simultaneously
+// then runs cacheBust, then watch task
+exports.default = series(
+  parallel(scssTask, jsTask),
+  cacheBustTask,
+  watchTask
+);
